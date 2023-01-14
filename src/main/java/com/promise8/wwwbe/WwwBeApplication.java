a514ffa017174f9589388c3b2c3744ac5a4ca356
kohen.kang <tjdcks1781@gmail.com>
2023-01-14 16:43:22 +0900

    [Nexters/www-be#1] project setup

---
+++ src/main/java/com/promise8/wwwbe/WwwBeApplication.java
@@ -0,0 +1,13 @@
+package com.promise8.wwwbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/promise8/wwwbe/WwwBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promise8.wwwbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
