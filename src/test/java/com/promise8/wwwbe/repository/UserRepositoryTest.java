c4c8fe807d7370864dbf24370fd5c8636e73a81e
emost22 <emoney96@naver.com>
2023-01-18 11:08:39 +0900

    [Nexters/www-be#14] Fix profile local -> alpha in UserRepositoryTest

--- src/test/java/com/promise8/wwwbe/repository/UserRepositoryTest.java
+++ src/test/java/com/promise8/wwwbe/repository/UserRepositoryTest.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @SpringBootTest
-@ActiveProfiles("local")
+@ActiveProfiles("alpha")
 class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
