80ff16e2deec08187318b928a73f2c5ba2fce4a7
emost22 <emoney96@naver.com>
2023-01-25 11:20:23 +0900

    [Nexters/www-be#6] Add dynamicLink

---
+++ src/main/java/com/promise8/wwwbe/model/dto/DynamicLinkReqDto.java
@@ -0,0 +1,12 @@
+package com.promise8.wwwbe.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DynamicLinkReqDto {
+    private String longDynamicLink;
+}
--- src/main/java/com/promise8/wwwbe/service/MeetingService.java
+++ src/main/java/com/promise8/wwwbe/service/MeetingService.java
@@ -1,5 +1,6 @@
 package com.promise8.wwwbe.service;
 
+import com.promise8.wwwbe.model.dto.DynamicLinkReqDto;
 import com.promise8.wwwbe.model.dto.MeetingCreateReqDto;
 import com.promise8.wwwbe.model.dto.MeetingCreateResDto;
 import com.promise8.wwwbe.model.dto.PromiseDateAndTimeDto;
@@ -7,9 +8,18 @@ import com.promise8.wwwbe.model.entity.*;
 import com.promise8.wwwbe.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,12 +27,20 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MeetingService {
     private final int MEETING_CODE_LENGTH = 6;
+    private final String androidPackage = "com.promiseeight.www";
+    private final String iosPackage = "com.promise8.www";
+    // TODO Fix link
+    private final String longDynamicLink = "https://whenwheres.page.link/?link=https://naver.com";
     private final PushService pushService;
     private final UserRepository userRepository;
     private final MeetingRepository meetingRepository;
     private final MeetingUserRepository meetingUserRepository;
     private final MeetingUserTimetableRepository meetingUserTimetableRepository;
     private final MeetingPlaceRepository meetingPlaceRepository;
+    @Value("${api-key.android}")
+    private String androidApiKey;
+    @Value("${api-key.ios}")
+    private String iosApiKey;
 
     public MeetingCreateResDto createMeeting(MeetingCreateReqDto meetingCreateReqDto) {
         String meetingCode = RandomStringUtils.random(MEETING_CODE_LENGTH, true, false);
@@ -47,7 +65,8 @@ public class MeetingService {
             meetingPlaceRepository.save(new MeetingPlaceEntity(promisePlace, meetingUserEntity));
         }
 
-        return new MeetingCreateResDto(meetingCode, null);
+        // TODO Fix deviceType
+        return new MeetingCreateResDto(meetingCode, getDynamicLink(true));
     }
 
     private UserEntity getUser(String deviceId, String userName) {
@@ -55,4 +74,25 @@ public class MeetingService {
         return optionalUserEntity.orElseGet(() -> userRepository.save(new UserEntity(deviceId, userName)));
     }
 
+    private String getDynamicLink(boolean deviceType) {
+        String dynamicLinkUrl = "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=";
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-type", "application/json");
+        MultiValueMap<String, String> httpBody = new LinkedMultiValueMap<>();
+        if (deviceType) {
+            httpBody.add("longDynamicLink", longDynamicLink + "&apn=" + androidPackage);
+            dynamicLinkUrl += androidApiKey;
+        } else {
+            httpBody.add("longDynamicLink", longDynamicLink + "&ibi=" + iosPackage);
+            dynamicLinkUrl += iosApiKey;
+        }
+
+        DynamicLinkReqDto dynamicLinkReqDto = new DynamicLinkReqDto(longDynamicLink + "&apn=" + androidPackage);
+        HttpEntity<DynamicLinkReqDto> dynamicLinkReq = new HttpEntity<>(dynamicLinkReqDto, httpHeaders);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<HashMap> response = restTemplate.exchange(dynamicLinkUrl, HttpMethod.POST, dynamicLinkReq, HashMap.class);
+
+        return response.getBody().get("shortLink").toString();
+    }
 }
