757ff5baf659e2f20cc4e8847ca3874457f6417c
emost22 <emoney96@naver.com>
2023-01-29 23:18:59 +0900

    [Nexters/www-be#6] Fix getMeetingList getDeviceId method

--- src/main/java/com/promise8/wwwbe/controller/MeetingController.java
+++ src/main/java/com/promise8/wwwbe/controller/MeetingController.java
@@ -1,24 +1,22 @@
 package com.promise8.wwwbe.controller;
 
-import com.promise8.wwwbe.config.security.TokenProvider;
+import com.promise8.wwwbe.config.security.UserPrincipal;
 import com.promise8.wwwbe.model.dto.MeetingCreateReqDto;
 import com.promise8.wwwbe.model.dto.MeetingCreateResDto;
 import com.promise8.wwwbe.model.dto.MeetingGetRes;
 import com.promise8.wwwbe.model.http.BaseResponse;
 import com.promise8.wwwbe.service.MeetingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
 @RequestMapping("/meetings")
 @RequiredArgsConstructor
 public class MeetingController {
-    private static final int TOKEN_BEGIN_INDEX = 7;
     private final MeetingService meetingService;
-    private final TokenProvider tokenProvider;
 
     /**
      * 입력 받을 정보 : 방 이름, 예상인원, 일정 범위, 투표 종료일
@@ -31,11 +29,8 @@ public class MeetingController {
     }
 
     @GetMapping
-    public BaseResponse<List<MeetingGetRes>> getMeetingList(HttpServletRequest request) {
-        String token = request.getHeader("Authorization").substring(TOKEN_BEGIN_INDEX);
-        String deviceId = tokenProvider.getDeviceIdFromToken(token);
-
-        return BaseResponse.ok(meetingService.getMeetingByDeviceId(deviceId));
+    public BaseResponse<List<MeetingGetRes>> getMeetingList(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        return BaseResponse.ok(meetingService.getMeetingByDeviceId(userPrincipal.getDeviceId()));
     }
 
     @GetMapping("/{meetingId}")
