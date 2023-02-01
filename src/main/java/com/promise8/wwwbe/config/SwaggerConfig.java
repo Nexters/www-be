c0e30826c3e2aadd44db7303fe48dd7a4d2be960
emost22 <emoney96@naver.com>
2023-02-01 20:18:00 +0900

    [Nexters/www-be#35] Add authorization in swagger

--- src/main/java/com/promise8/wwwbe/config/SwaggerConfig.java
+++ src/main/java/com/promise8/wwwbe/config/SwaggerConfig.java
@@ -1,22 +1,29 @@
 package com.promise8.wwwbe.config;
 
+import org.apache.http.HttpHeaders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    public static final String AUTHORIZATION_SCOPE_GLOBAL = "global";
+    public static final String AUTHORIZATION_SCOPE_GLOBAL_DESC = "accessEverything";
+    private static final String SECURITY_SCHEMA_NAME = "JWT";
 
     @Bean
     public Docket api() {
@@ -27,7 +34,26 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.promise8.wwwbe.controller"))
                 .paths(PathSelectors.ant("/**"))
+                .build()
+                .securityContexts(newArrayList(securityContext()))
+                .securitySchemes(newArrayList(apiKey()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey(SECURITY_SCHEMA_NAME, HttpHeaders.AUTHORIZATION, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
                 .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope(AUTHORIZATION_SCOPE_GLOBAL, AUTHORIZATION_SCOPE_GLOBAL_DESC);
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return newArrayList(new SecurityReference(SECURITY_SCHEMA_NAME, authorizationScopes));
     }
 
     private Set<String> getConsumeContentTypes() {
