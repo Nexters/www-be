1e4ce2a8279c6b0c8343b2509f0fcf681e494c24
emost22 <emoney96@naver.com>
2023-02-08 00:31:20 +0900

    [Nexters/www-be#51] Add priority in enums

--- src/main/java/com/promise8/wwwbe/model/dto/PromiseTime.java
+++ src/main/java/com/promise8/wwwbe/model/dto/PromiseTime.java
@@ -4,8 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum PromiseTime {
-    MORNING,
-    LUNCH,
-    DINNER,
-    NIGHT;
+    MORNING("MORNING", 0),
+    LUNCH("LUNCH", 1),
+    DINNER("DINNER", 2),
+    NIGHT("NIGHT", 3);
+
+    private String time;
+    private int priority;
+
+    PromiseTime(String time, int priority) {
+        this.time = time;
+        this.priority = priority;
+    }
 }
--- src/main/java/com/promise8/wwwbe/model/entity/MeetingStatus.java
+++ src/main/java/com/promise8/wwwbe/model/entity/MeetingStatus.java
@@ -1,19 +1,19 @@
 package com.promise8.wwwbe.model.entity;
 
+import lombok.Getter;
+
 import java.util.Arrays;
 
+@Getter
 public enum MeetingStatus {
-    WAITING("waiting"), // 투표 시작 전 (생성 시점)
-    VOTING("voting"), // 투표 중 (방 최소 조건 인원 달성시)
-    VOTED("voted"), // 투표 완료 (방장 액션에 의해)
-    CONFIRMED("confirmed"), // 약속 확정 (방자 액션에 의해)
-    DONE("done"); // 약속 종료 (약속 확정 날짜 지난 경우)
+    WAITING("waiting", 0), // 투표 시작 전 (생성 시점)
+    VOTING("voting", 1), // 투표 중 (방 최소 조건 인원 달성시)
+    VOTED("voted", 2), // 투표 완료 (방장 액션에 의해)
+    CONFIRMED("confirmed", 3), // 약속 확정 (방자 액션에 의해)
+    DONE("done", 4); // 약속 종료 (약속 확정 날짜 지난 경우)
 
     private String name;
-
-    MeetingStatus(String name) {
-        this.name = name;
-    }
+    private int priority;
 
     public static MeetingStatus of(String meetingStatusStr) {
         if (meetingStatusStr == null) {
@@ -26,6 +26,11 @@ public enum MeetingStatus {
                 .orElseThrow(() -> {
                     throw new IllegalArgumentException("not valid argument. " + meetingStatusStr);
                 });
+    }
+
+    MeetingStatus(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
     }
 
 }
