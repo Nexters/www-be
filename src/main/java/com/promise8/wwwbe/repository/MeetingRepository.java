fd1c27af10447f0f464941392d2bd97bee8d0767
emost22 <emoney96@naver.com>
2023-02-09 03:02:23 +0900

    [Nexters/www-be#56] Add Scheduler for change MeetingStatus

--- src/main/java/com/promise8/wwwbe/repository/MeetingRepository.java
+++ src/main/java/com/promise8/wwwbe/repository/MeetingRepository.java
@@ -4,6 +4,7 @@ import com.promise8.wwwbe.model.entity.MeetingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +14,9 @@ public interface MeetingRepository extends JpaRepository<MeetingEntity, Long> {
 
     public Optional<MeetingEntity> findByMeetingCode(String meetingCode);
 
-    @Query(value = "select * from meeting m join meeting_user mu join user u on u.device_id = ?1 and mu.user_id = u.user_id and mu.meeting_id = m.meeting_id;", nativeQuery = true)
+    @Query(value = "select * from meeting m join meeting_user mu join user u on u.device_id = ?1 and mu.user_id = u.user_id and mu.meeting_id = m.meeting_id", nativeQuery = true)
     public List<MeetingEntity> findByUserEntity_DeviceId(String deviceId);
+
+    @Query(value = "select * from meeting m join meeting_user mu join meeting_user_timetable mut on mut.promise_date < ?1 and mut.is_confirmed = ?2 and mut.meeting_user_id = mu.meeting_user_id and mu.meeting_id = m.meeting_id and m.meeting_status = ?3", nativeQuery = true)
+    public List<MeetingEntity> findByMeetingStatusAndConfirmedDate(LocalDate nowDate, boolean isConfirmed, String meetingStatus);
 }
