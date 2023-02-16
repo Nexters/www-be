8d26f631acc2f9715a1746a89cba140cf11a373a
YongSoo Kang <emoney96@naver.com>
2023-02-17 00:26:48 +0900

    [Nexters/www-be#73] Add api description for swagger (#74)

--- src/main/java/com/promise8/wwwbe/service/UserService.java
+++ src/main/java/com/promise8/wwwbe/service/UserService.java
@@ -3,6 +3,7 @@ package com.promise8.wwwbe.service;
 import com.promise8.wwwbe.model.entity.UserEntity;
 import com.promise8.wwwbe.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,7 +14,8 @@ public class UserService {
 
     @Transactional
     public void setAlarm(long userId, boolean isAlarmOn) {
-        UserEntity userEntity = userRepository.findById(userId).orElseThrow();
+        UserEntity userEntity = userRepository.findById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with userId : " + userId));
         userEntity.setIsAlarmOn(isAlarmOn);
     }
 }
