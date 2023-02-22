eafb6145c4298c3337e22c4a0b0b7fe5eb8ec6ed
emost22 <emoney96@naver.com>
2023-02-23 00:58:40 +0900

    [Nexters/www-be#77] Add getTimetableList

--- src/main/java/com/promise8/wwwbe/service/MeetingServiceHelper.java
+++ src/main/java/com/promise8/wwwbe/service/MeetingServiceHelper.java
@@ -1,11 +1,18 @@
 package com.promise8.wwwbe.service;
 
+import com.promise8.wwwbe.model.dto.PromiseDayOfWeek;
+import com.promise8.wwwbe.model.dto.PromiseTime;
 import com.promise8.wwwbe.model.dto.res.ConfirmedPromiseResDto;
+import com.promise8.wwwbe.model.dto.res.UserPromiseTimeResDto;
+import com.promise8.wwwbe.model.entity.MeetingEntity;
 import com.promise8.wwwbe.model.entity.MeetingPlaceEntity;
 import com.promise8.wwwbe.model.entity.MeetingUserEntity;
 import com.promise8.wwwbe.model.entity.MeetingUserTimetableEntity;
 import org.springframework.stereotype.Component;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -61,5 +68,48 @@ public class MeetingServiceHelper {
         }
 
         return confirmedPromiseResDto;
+    }
+
+    public static List<UserPromiseTimeResDto> getUserPromiseTimeList(MeetingEntity meetingEntity) {
+        List<UserPromiseTimeResDto> userPromiseTimeResDtoList = new ArrayList<>();
+        if (meetingEntity.getMeetingUserEntityList() == null || meetingEntity.getMeetingUserEntityList().isEmpty()) {
+            return userPromiseTimeResDtoList;
+        }
+
+        meetingEntity.getMeetingUserEntityList().forEach(meetingUser -> {
+            meetingUser.getMeetingUserTimetableEntityList().forEach(res -> {
+                LocalDate promiseDate = res.getPromiseDate();
+                PromiseTime promiseTime = res.getPromiseTime();
+
+                boolean isAdd = false;
+                for (UserPromiseTimeResDto userPromiseTime : userPromiseTimeResDtoList) {
+                    if (userPromiseTime.getPromiseDate().equals(promiseDate) && userPromiseTime.getPromiseTime().equals(promiseTime)) {
+                        userPromiseTime.getUserNameList().add(meetingUser.getMeetingUserName());
+                        isAdd = true;
+                        break;
+                    }
+                }
+
+                if (!isAdd) {
+                    List<String> userNameList = new ArrayList<>();
+                    userNameList.add(meetingUser.getMeetingUserName());
+                    userPromiseTimeResDtoList.add(UserPromiseTimeResDto.builder()
+                            .promiseDate(promiseDate)
+                            .promiseTime(promiseTime)
+                            .promiseDayOfWeek(getPromiseDayOfWeek(promiseDate))
+                            .userNameList(userNameList)
+                            .build());
+                }
+            });
+        });
+
+        userPromiseTimeResDtoList.sort((o1, o2) -> o2.getUserNameList().size() - o1.getUserNameList().size());
+        return userPromiseTimeResDtoList;
+    }
+
+    private static PromiseDayOfWeek getPromiseDayOfWeek(LocalDate promiseDate) {
+        DayOfWeek dayOfWeek = promiseDate.getDayOfWeek();
+        int idx = dayOfWeek.getValue();
+        return PromiseDayOfWeek.values()[idx - 1];
     }
 }
