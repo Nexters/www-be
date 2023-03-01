a800884eb50724138cb5e0c5285dd92e63f32707
YongSoo Kang <emoney96@naver.com>
2023-03-01 21:08:07 +0900

    [Nexters/www-be#77] Add voting status check (#134)

--- src/main/java/com/promise8/wwwbe/service/MeetingAuthorizer.java
+++ src/main/java/com/promise8/wwwbe/service/MeetingAuthorizer.java
@@ -2,6 +2,7 @@ package com.promise8.wwwbe.service;
 
 import com.promise8.wwwbe.config.security.UserPrincipal;
 import com.promise8.wwwbe.model.entity.MeetingEntity;
+import com.promise8.wwwbe.model.entity.MeetingStatus;
 import com.promise8.wwwbe.model.entity.UserEntity;
 import com.promise8.wwwbe.model.exception.BizException;
 import com.promise8.wwwbe.model.http.BaseErrorCode;
@@ -24,5 +25,17 @@ public class MeetingAuthorizer {
         });
         UserEntity creator = meetingEntity.getCreator();
         return creator.getUserId() == userPrincipal.getId();
+    }
+
+    public boolean isMeetingVoting(long meetingId) {
+        MeetingEntity meetingEntity = meetingRepository.findById(meetingId).orElseThrow(() -> {
+            throw new BizException(BaseErrorCode.NOT_EXIST_MEETING);
+        });
+
+        if (!MeetingStatus.VOTING.equals(meetingEntity.getMeetingStatus())) {
+            throw new BizException(BaseErrorCode.NOT_MEETING_STATUS_VOTING);
+        }
+
+        return true;
     }
 }
